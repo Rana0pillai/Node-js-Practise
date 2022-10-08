481bb8a9ccc16c1a6bf2a9d2bf18613a1b774198
Rana pillai <ranamaripillai@gmail.com>
2022-10-08 23:42:14 +0530

    lecture-32(complited)

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
-const http = require('http')
+const http = require('http');
 const server = http.createServer((req,res)=>{
-    console.log(req)
+    // console.log(req.url, req.method, req.headers);
+    const url = req.url;
+    if(url === "/"){
+    res.setHeader('Content-Type','text/html');
+    res.write('<html><head><title>Js Page</title><body><form action="/message" method="POST"><button type="submit">Go to NEXT Page</button></form></body></head></html>');
+    return res.end();
+    }
+
+    res.setHeader('Content-Type','text/html');
+    res.write('<html><head><title>Js Page</title><body><h1>Welcome to node.js Mr.Rana Pillai</h1></body></head></html>');
+    res.end();
+
+    
+    // process.exit();
 })
 server.listen('2000')
